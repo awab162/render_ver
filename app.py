e3daf26a277777e51dca421997e963cbc19edfda
acs.ust.edu.sd/ <127499589+awab162@users.noreply.github.com>
2026-05-07 14:15:27 +0200

    1.2

--- app.py
+++ app.py
@@ -590,11 +590,13 @@ class VideoDownloader:
             url = f'https://www.youtube.com/watch?v={video_id}'
             
             ydl_opts = {
-                'format': f'bestaudio[abr<={quality}][ext=m4a]/bestaudio[abr<={quality}][ext=webm]/bestaudio[abr<={quality}]/bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio/best',
+                # Simple & permissive: grab ANY audio stream, FFmpeg converts to MP3
+                'format': 'bestaudio/best',
                 'outtmpl': str(output_template_path),
                 'noplaylist': True,
                 'writethumbnail': False,
                 'writeinfojson': False,
+                'no_check_certificate': True,
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
                     'preferredcodec': 'mp3',
